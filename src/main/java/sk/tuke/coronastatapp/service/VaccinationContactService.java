15d200819f15bb5044524c7101264e6539bd422f
lukasdancak <lukas.dancak@gmail.com>
2022-09-08 15:27:02 +0200

    vytrvorena trieda VaccinationContactServiceJPA.java

--- src/main/java/sk/tuke/coronastatapp/service/VaccinationContactService.java
+++ src/main/java/sk/tuke/coronastatapp/service/VaccinationContactService.java
@@ -1,4 +1,12 @@
 package sk.tuke.coronastatapp.service;
 
+import sk.tuke.coronastatapp.entity.VaccinationContact;
+
+import java.util.List;
+
 public interface VaccinationContactService {
+
+    void addVaccination(VaccinationContact vaccinationContact);
+
+    List<VaccinationContact> getAllVaccinationContacts();
 }
--- src/main/java/sk/tuke/coronastatapp/service/VaccinationContactServiceJPA.java
+++ src/main/java/sk/tuke/coronastatapp/service/VaccinationContactServiceJPA.java
@@ -1,27 +1,26 @@
 package sk.tuke.coronastatapp.service;
 
 import sk.tuke.coronastatapp.entity.Vaccination;
+import sk.tuke.coronastatapp.entity.VaccinationContact;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-
 import java.util.List;
 
 @Transactional
-public class VaccinationServiceJPA implements VaccinationService {
+public class VaccinationContactServiceJPA implements VaccinationContactService {
 
     @PersistenceContext
     private EntityManager entityManager;
 
     @Override
-    public void addVaccination(Vaccination vaccination) {
+    public void addVaccination(VaccinationContact vaccinationContact) {
 
-        entityManager.persist(vaccination);
     }
 
     @Override
-    public List<Vaccination> getVaccinations() {
-        return entityManager.createQuery("select v from Vaccination v").getResultList();
+    public List<VaccinationContact> getAllVaccinationContacts() {
+        return null;
     }
 }

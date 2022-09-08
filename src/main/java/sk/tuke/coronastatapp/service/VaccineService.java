48dc012bf08337617912e5b620dca123de5032d6
lukasdancak <lukas.dancak@gmail.com>
2022-09-08 14:56:07 +0200

    VaccineService interface pridane

---
+++ src/main/java/sk/tuke/coronastatapp/service/VaccineService.java
@@ -0,0 +1,14 @@
+package sk.tuke.coronastatapp.service;
+
+import sk.tuke.coronastatapp.entity.Vaccine;
+
+import java.util.List;
+
+public interface VaccineService {
+
+    void addVaccine(Vaccine vaccine);
+
+    List<Vaccine> getVaccines();
+
+    Vaccine getVaccineById(int vaccineId);
+}

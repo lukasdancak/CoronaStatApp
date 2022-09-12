7815103ec7a904390a0b18855e13eb3341d6c0f0
lukasdancak <lukas.dancak@gmail.com>
2022-09-12 13:38:50 +0200

    praca na DbAdministrationController, css, tabulka vytvorena

--- src/main/java/sk/tuke/coronastatapp/server/controller/DbAdministrationController.java
+++ src/main/java/sk/tuke/coronastatapp/server/controller/DbAdministrationController.java
@@ -3,6 +3,8 @@ package sk.tuke.coronastatapp.server.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.WebApplicationContext;
 import sk.tuke.coronastatapp.service.VaccinationContactService;
 
@@ -12,6 +14,24 @@ public class DbAdministrationController {
 
     @Autowired
     VaccinationContactService vaccinationContactService;
+
+
+    @RequestMapping("/dbadministration")
+    public String dbAdministration(Model model) {
+        prepareModel(model);
+        return "dbadministration";
+    }
+
+    private void prepareModel(Model model) {
+        int vaccinationContactLocalSize = -1; // ak ostane -1, znamena to problem s databazou
+        try {
+            vaccinationContactLocalSize = vaccinationContactService.getAllVaccinationContacts().size();
+        } catch (Exception e) {
+            //e.printStackTrace();
+        }
+
+        model.addAttribute("VaccinationContactLocalSize", vaccinationContactLocalSize);
+    }
 
 
 }

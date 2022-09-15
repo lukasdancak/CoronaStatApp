b24292fd4b44b5693a24dd95008189d378f26db2
lukasdancak <lukas.dancak@gmail.com>
2022-09-15 17:09:35 +0200

    dokoncena naplnanie tabuliek VaccinationContacts a SlovakiaVaccination + ovladanie

--- src/main/java/sk/tuke/coronastatapp/server/controller/DbAdministrationController.java
+++ src/main/java/sk/tuke/coronastatapp/server/controller/DbAdministrationController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
+import sk.tuke.coronastatapp.entity.SlovakiaVaccination;
+import sk.tuke.coronastatapp.entity.VaccinationContact;
 import sk.tuke.coronastatapp.service.RegionService;
 import sk.tuke.coronastatapp.service.SlovakiaVaccinationService;
 import sk.tuke.coronastatapp.service.VaccinationContactService;
@@ -84,22 +86,57 @@ public class DbAdministrationController {
 
 
     @RequestMapping("/dbadministration/clear")
-    public String clearTable(@RequestParam(required = false) String tableName,
+    public String clearTable(@RequestParam(required = false) String tablename,
                              Model model) {
-        if (tableName == null) {
+        System.out.println("som v clearTable metode");
+        //tu by mala byt kontrola, ci je prihlaseny admin
+
+        if (tablename == null) {
+            System.out.println("tablename is null");
             return "redirect:/dbadministration";
         }
+        System.out.println("som v clearTable metode po kontrole tableName is null ?");
 
-        switch (tableName) {
-            case "vaccinationcontacts":
+        switch (tablename) {
+            case "vaccinationcontact":
                 vaccinationContactService.deleteAllVaccinationContacts();
+                System.out.println("som vo switchi, vaccination contact");
                 break;
 
-            case "slovakiavaccsinationservices":
+            case "slovakiavaccination":
                 slovakiaVaccinationService.deleteAllSlovakiaVaccinations();
                 break;
         }
-        
+
+        return "redirect:/dbadministration";
+    }
+
+    @RequestMapping("/dbadministration/clearandfill")
+    public String clearAndFillTable(@RequestParam(required = false) String tablename,
+                                    Model model) {
+        //tu by mala byt kontrola, ci je prihlaseny admin
+        if (tablename == null) {
+            return "redirect:/dbadministration";
+        }
+
+        switch (tablename) {
+            case "vaccinationcontact":
+                vaccinationContactService.deleteAllVaccinationContacts();
+                var list = vaccinationContactGovDb.getAllVaccinationContacts();
+                for (VaccinationContact vc : list) {
+                    vaccinationContactService.addVaccination(vc);
+                }
+                break;
+
+            case "slovakiavaccination":
+                slovakiaVaccinationService.deleteAllSlovakiaVaccinations();
+                var list2 = slovakiaVaccinationGovDb.getAllSlovakiaVaccinations();
+                for (SlovakiaVaccination sv : list2) {
+                    slovakiaVaccinationService.addSlovakiaVaccination(sv);
+                }
+                break;
+        }
+
         return "redirect:/dbadministration";
     }
 

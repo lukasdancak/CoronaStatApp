1653edbe853866020de1b15562c342cef2982456
lukasdancak <lukas.dancak@gmail.com>
2022-09-15 13:52:59 +0200

    VaccinationContactGovDbImplJACKSON  -osetreni offset

--- src/main/java/sk/tuke/coronastatapp/service/lukatestservices/VaccinationContactGovDbImplJACKSON.java
+++ src/main/java/sk/tuke/coronastatapp/service/lukatestservices/VaccinationContactGovDbImplJACKSON.java
@@ -9,6 +9,7 @@ import sk.tuke.coronastatapp.entity.VaccinationContact;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VaccinationContactGovDbImplJACKSON implements VaccinationContactGovDb {
@@ -18,19 +19,27 @@ public class VaccinationContactGovDbImplJACKSON implements VaccinationContactGov
 
         try {
             String URLString = "https://data.korona.gov.sk/api/vaccination/contacts";
+            List<VaccinationContact> vaccinationContacts = new ArrayList<VaccinationContact>();
+            List<VaccinationContact> tempVaccinationContacts = new ArrayList<VaccinationContact>();
             URL url = new URL(URLString);
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode rootNode = mapper.readTree(url);
-            JsonNode node = rootNode.path("page");
-            JsonParser parser = mapper.treeAsTokens(node);
-            TypeReference<List<VaccinationContact>> typeReference = new TypeReference<>() {
-            };
-            List<VaccinationContact> districtAgTests = mapper.readValue(parser, typeReference);
-//            for (int i = 0; i < districtAgTests.size(); i++) {
-//                districtAgTestsService.addDistrictAgTests(districtAgTests.get(i));
-//            }
+
+            do {
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode rootNode = mapper.readTree(url);
+                JsonNode node = rootNode.path("page");
+                JsonParser parser = mapper.treeAsTokens(node);
+                TypeReference<List<VaccinationContact>> typeReference = new TypeReference<>() {
+                };
+                tempVaccinationContacts = mapper.readValue(parser, typeReference);
+                vaccinationContacts.addAll(tempVaccinationContacts);
+                /***create next link, for next offset*/
+                String offset = rootNode.path("offset").textValue();
+                url = new URL(URLString + "?offset=" + offset);
+
+            } while (!tempVaccinationContacts.isEmpty());
+
             System.out.println("nacital som do listu vaccinationContacts");
-            return districtAgTests;
+            return vaccinationContacts;
         } catch (IOException e) {
             System.out.println(e.getMessage());
             System.out.println("problem s nacitanim vaccinationcontacts");
